2efa0b71b434b2aa0a00d1e64bf0e5bbc3bf9861
Ritchey <richard@mypatricia.co>
2023-02-28 16:19:51 +0100

    feature(animation): add animation to how it works section, update how it works section on mobile

--- components/Home/HowItWorks.vue
+++ components/Home/HowItWorks.vue
@@ -1,46 +1,105 @@
 <template>
-  <div id="how-it-works" class="how-it-works pb-6 hidden sm:block">
+  <div
+    v-observe-visibility="{
+      callback: visibilityChanged,
+      once: true,
+      intersection: {
+        threshold: 0.6,
+      },
+    }"
+    id="how-it-works"
+    class="how-it-works pb-6 hidden sm:block"
+  >
     <div class="flex justify-center pt-16">
       <HomeToggleButton v-model="switchValue" />
     </div>
 
     <div class="sm:w-11/12 lg:w-9/12 flex justify-center items-center m-auto mt-16 mb-4">
       <div class="w-3/12">
-        <div class="mb-32">
-          <div class="mr-0 m-auto step-box">{{ howItWorkSteps[activeTab][0].step }}</div>
-          <h6 class="text-sm md:text-base font-medium text-right leading-5 mb-2">
+        <div class="mb-32" :class="{ hidden: !resetAnimation }">
+          <div
+            class="mr-0 m-auto step-box opacity-0"
+            :class="{ 'opacity-1 animate__animated animate__fadeInUp': isVisible }"
+          >
+            {{ howItWorkSteps[activeTab][0].step }}
+          </div>
+          <h6
+            class="text-sm md:text-base font-medium text-right leading-5 mb-2 opacity-0"
+            :class="{ 'opacity-1 animate__animated animate__fadeInUp animate__delay-500ms': isVisible }"
+          >
             {{ howItWorkSteps[activeTab][0].title }}
           </h6>
-          <p class="text-xs text-right">
+          <p
+            class="text-xs text-right opacity-0"
+            :class="{ 'opacity-1 animate__animated animate__fadeInUp animate__delay-1s': isVisible }"
+          >
             {{ howItWorkSteps[activeTab][0].description }}
           </p>
         </div>
-        <div>
-          <div class="mr-0 m-auto step-box">{{ howItWorkSteps[activeTab][2].step }}</div>
-          <h6 class="text-sm md:text-base font-medium text-right leading-5 mb-2">
+
+        <div :class="{ hidden: !resetAnimation }">
+          <div
+            class="mr-0 m-auto step-box opacity-0"
+            :class="{ 'opacity-1 animate__animated animate__fadeInUp animate__delay-3s': isVisible }"
+          >
+            {{ howItWorkSteps[activeTab][2].step }}
+          </div>
+          <h6
+            class="text-sm md:text-base font-medium text-right leading-5 mb-2 opacity-0"
+            :class="{ 'opacity-1 animate__animated animate__fadeInUp animate__delay-3500ms': isVisible }"
+          >
             {{ howItWorkSteps[activeTab][2].title }}
           </h6>
-          <p class="text-xs text-right">{{ howItWorkSteps[activeTab][2].description }}</p>
+          <p
+            class="text-xs text-right opacity-0"
+            :class="{ 'opacity-1 animate__animated animate__fadeInUp animate__delay-4s': isVisible }"
+          >
+            {{ howItWorkSteps[activeTab][2].description }}
+          </p>
         </div>
       </div>
 
       <nuxt-img class="w-5/12 mx-10" src="/images/how-screenshot.png" />
 
       <div class="w-3/12">
-        <div class="mb-40">
-          <div class="ml-0 m-auto step-box">{{ howItWorkSteps[activeTab][1].step }}</div>
-          <h6 class="text-sm md:text-base font-medium text-left leading-5 mb-2">
+        <div class="mb-40" :class="{ hidden: !resetAnimation }">
+          <div
+            class="ml-0 m-auto step-box opacity-0"
+            :class="{ 'opacity-1 animate__animated animate__fadeInUp animate__delay-1500ms': isVisible }"
+          >
+            {{ howItWorkSteps[activeTab][1].step }}
+          </div>
+          <h6
+            class="text-sm md:text-base font-medium text-left leading-5 mb-2 opacity-0"
+            :class="{ 'opacity-1 animate__animated animate__fadeInUp animate__delay-2s': isVisible }"
+          >
             {{ howItWorkSteps[activeTab][1].title }}
           </h6>
-          <p class="text-xs text-left">{{ howItWorkSteps[activeTab][1].description }}</p>
+          <p
+            class="text-xs text-left opacity-0"
+            :class="{ 'opacity-1 animate__animated animate__fadeInUp animate__delay-2500ms': isVisible }"
+          >
+            {{ howItWorkSteps[activeTab][1].description }}
+          </p>
         </div>
 
-        <div>
-          <div class="ml-0 m-auto step-box">{{ howItWorkSteps[activeTab][3].step }}</div>
-          <h6 class="text-sm md:text-base font-medium text-left leading-5 mb-2">
+        <div :class="{ hidden: !resetAnimation }">
+          <div
+            class="ml-0 m-auto step-box opacity-0"
+            :class="{ 'opacity-1 animate__animated animate__fadeInUp animate__delay-4500ms': isVisible }"
+          >
+            {{ howItWorkSteps[activeTab][3].step }}
+          </div>
+          <h6
+            class="text-sm md:text-base font-medium text-left leading-5 mb-2 opacity-0"
+            :class="{ 'opacity-1 animate__animated animate__fadeInUp animate__delay-5s': isVisible }"
+          >
             {{ howItWorkSteps[activeTab][3].title }}
           </h6>
-          <p class="text-xs text-left">
+          <p
+            class="text-xs text-left opacity-0"
+            :class="{ 'opacity-1 animate__animated animate__fadeInUp animate__delay-5500ms': isVisible }"
+          >
             {{ howItWorkSteps[activeTab][3].description }}
           </p>
         </div>
@@ -52,9 +111,26 @@
 <script lang="ts" setup>
 import { useHowItWorkSteps } from "~/composables/howItWorkSteps"
 
+const resetAnimation = ref<boolean>(true)
 const switchValue = ref<boolean>(false)
 const activeTab = computed<string>(() => (switchValue.value ? "driver" : "carOwners"))
+
 const howItWorkSteps = useHowItWorkSteps()
+
+const isVisible = ref<boolean>(false)
+const visibilityChanged = (_isVisible: boolean, entry: any) => {
+  isVisible.value = _isVisible
+}
+
+let timeoutId: number
+watch(switchValue, () => {
+  resetAnimation.value = false
+  timeoutId = setTimeout(() => (resetAnimation.value = true), 100)
+})
+
+onBeforeUnmount(() => {
+  clearTimeout(timeoutId)
+})
 </script>
 
 <style lang="scss" scoped>
--- components/Home/HowItWorksSmall.vue
+++ components/Home/HowItWorksSmall.vue
@@ -1,15 +1,59 @@
 <template>
-  <div id="how-it-works" class="how-it-works pb-6 sm:hidden">
+  <div
+    v-observe-visibility="{
+      callback: visibilityChanged,
+      once: true,
+      intersection: {
+        threshold: 0.6,
+      },
+    }"
+    id="how-it-works-m"
+    class="how-it-works pb-6 sm:hidden"
+  >
     <div class="flex justify-center">
       <HomeToggleButton v-model="switchValue" is-mobile class="mt-16" />
     </div>
 
     <div class="w-11/12 sm:w-11/12 lg:w-9/12 flex justify-between sm:justify-center items-center m-auto mt-16 mb-4">
-      <div class="w-6/12">
-        <div class="mb-4" v-for="howItWorkStep in howItWorkSteps[activeTab]">
-          <div class="mr-0 m-auto step-box">{{ howItWorkStep.step }}</div>
-          <h6 class="text-sm font-medium text-right leading-5">{{ howItWorkStep.title }}</h6>
-          <p class="text-[11px] text-right">
+      <div class="w-6/12 min-h-[570px]">
+        <div
+          class="mb-4"
+          :class="{ hidden: !resetAnimation }"
+          v-for="(howItWorkStep, index) in howItWorkSteps[activeTab]"
+        >
+          <div
+            class="mr-0 m-auto step-box opacity-0"
+            :class="{
+              'opacity-1 animate__animated animate__fadeInUp': isVisible,
+              'animate__delay-1500ms': index === 1,
+              'animate__delay-3s': index === 2,
+              'animate__delay-4500ms': index === 3,
+            }"
+          >
+            {{ howItWorkStep.step }}
+          </div>
+          <h6
+            class="text-sm font-medium text-right leading-5 opacity-0"
+            :class="{
+              'opacity-1 animate__animated animate__fadeInUp': isVisible,
+              'animate__delay-500ms': index === 0,
+              'animate__delay-2s': index === 1,
+              'animate__delay-3500ms': index === 2,
+              'animate__delay-5s': index === 3,
+            }"
+          >
+            {{ howItWorkStep.title }}
+          </h6>
+          <p
+            class="text-[11px] text-right opacity-0"
+            :class="{
+              'opacity-1 animate__animated animate__fadeInUp': isVisible,
+              'animate__delay-1s': index === 0,
+              'animate__delay-2500ms': index === 1,
+              'animate__delay-4s': index === 2,
+              'animate__delay-5500ms': index === 3,
+            }"
+          >
             {{ howItWorkStep.description }}
           </p>
         </div>
@@ -23,9 +67,26 @@
 <script lang="ts" setup>
 import { useHowItWorkSteps } from "~/composables/howItWorkSteps"
 
+const resetAnimation = ref<boolean>(true)
 const switchValue = ref<boolean>(false)
 const activeTab = computed<string>(() => (switchValue.value ? "driver" : "carOwners"))
+
 const howItWorkSteps = useHowItWorkSteps()
+
+const isVisible = ref<boolean>(false)
+const visibilityChanged = (_isVisible: boolean, entry: any) => {
+  isVisible.value = _isVisible
+}
+
+let timeoutId: number
+watch(switchValue, () => {
+  resetAnimation.value = false
+  timeoutId = setTimeout(() => (resetAnimation.value = true), 100)
+})
+
+onBeforeUnmount(() => {
+  clearTimeout(timeoutId)
+})
 </script>
 
 <style lang="scss" scoped>
